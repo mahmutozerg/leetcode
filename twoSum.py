9cd86f61f70e7d4557905d55ecacc8556972f42b
Mahmut Özer Gözükırmızı <85442495+mahmutozerg@users.noreply.github.com>
2022-11-24 12:09:35 +0300

    Add files via upload

--- twoSum.py
+++ twoSum.py
@@ -1,22 +1,18 @@
-def twoSum(nums,target):
-    temp = nums.copy()
-    temp.sort()
-    i = 0
-    j = len(nums)-1
-    while 1:
-        if temp[i]+temp[j] == target:
-            if temp[i] == temp[j]:
-                return [nums.index(temp[i]),nums.index(temp[j],i+1)]
-            else:
-                return [nums.index(temp[i]),nums.index(temp[j])]
+class Solution:
+    def twoSum(self, nums: list, target: int) -> list:
+        temp = nums.copy()
+        temp.sort()
+        i = 0
+        j = len(nums)-1
+        while 1:
 
-        elif temp[i]+temp[j] > target:
-            j -=1
-        elif temp[i]+temp[j]<target:
-            i+=1
+            if temp[i]+temp[j] == target:
 
-
-
-print(twoSum([5,75,25],100))
-
-
+                if temp[i] == temp[j]:
+                    return [nums.index(temp[i]),nums.index(temp[j],i+1)]
+                else:
+                    return [nums.index(temp[i]),nums.index(temp[j])]
+            elif temp[i]+temp[j] > target:
+                j -=1
+            elif temp[i]+temp[j]<target:
+                i+=1
