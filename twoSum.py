f9eab74c76962a090b055d3dc4af9a6bad6f5ce5
Mahmut Özer Gözükırmızı <85442495+mahmutozerg@users.noreply.github.com>
2022-11-24 12:08:33 +0300

    Add files via upload

---
+++ twoSum.py
@@ -0,0 +1,22 @@
+def twoSum(nums,target):
+    temp = nums.copy()
+    temp.sort()
+    i = 0
+    j = len(nums)-1
+    while 1:
+        if temp[i]+temp[j] == target:
+            if temp[i] == temp[j]:
+                return [nums.index(temp[i]),nums.index(temp[j],i+1)]
+            else:
+                return [nums.index(temp[i]),nums.index(temp[j])]
+
+        elif temp[i]+temp[j] > target:
+            j -=1
+        elif temp[i]+temp[j]<target:
+            i+=1
+
+
+
+print(twoSum([5,75,25],100))
+
+
